b8f129a1592f913d371e2be90d85048e979efc18
Schluckauf-weg <zy_chen@outlook.com>
2023-04-13 16:54:26 +0800

    new logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: '简明德语语法',
   tagline: 'Deutsche Grammatik',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/de-c.png',
 
   // Set the production url of your site here
   url: 'https://schluckauf-weg.github.io',
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Schluckauf-weg/DeutscheGrammatik.github.io/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -56,19 +56,19 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      // image: 'img/',
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/de-s.png',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: '简明德语语法',
           },
           {
             href: 'https://github.com/Schluckauf-weg/DeutscheGrammatik.github.io.git',
