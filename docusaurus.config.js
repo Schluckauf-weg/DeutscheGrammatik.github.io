d9d05dddd83281329f0c6095d7afbe6d5b234590
Schluckauf-weg <zy_chen@outlook.com>
2023-07-10 12:32:41 +0800

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,23 +80,4 @@ const config = {
     }),
 };
 
-// module.exports = config;
-module.exports = {
-  // ... Your other configurations.
-  themes: [
-    // ... Your other themes.
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        language: ["de", "zh"],
-        // ```
-      }),
-    ],
-  ],
-};
+module.exports = config;
